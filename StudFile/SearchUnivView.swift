835592283c0d1121f47c435520400a54c0fe4cb7
Pleshakov <sanya.pleshakov.95@mail.ru>
2022-11-24 19:47:31 +0300

    fix

--- StudFile/SearchUnivView.swift
+++ StudFile/SearchUnivView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct SearchUnivView: View {
+    private var listOfUniv = ["u1", "u2", "u3"]
+    @State var searchText = ""
+    @State var buttonSearchColor = Color.red
+    @State var selectText = "Select"
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(universities, id: \.self) { univ in
+                    HStack {
+                        Button(action: {
+                            buttonSearchColor = Color.green
+                            selectText = univ.capitalized
+                            
+                        },
+                               label: {
+                            Text(univ.capitalized)
+                        })
+                    }
+                    .padding(.all, 10)
+                }
+                
+                
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("Universities")
+        }
     }
+    
+    var universities: [String] {
+        let lcUnivercites = listOfUniv.map { $0.lowercased() }
+        
+        return searchText == "" ? lcUnivercites : lcUnivercites.filter {
+            $0.contains(searchText.lowercased())
+        }
+        
+    }
+    
 }
 
 struct SearchUnivView_Previews: PreviewProvider {
