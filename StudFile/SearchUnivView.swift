fc6055acbc3e502a44a4108c99dd4218d01fd74e
Pleshakov <sanya.pleshakov.95@mail.ru>
2022-11-24 19:46:18 +0300

    Added search of university

---
+++ StudFile/SearchUnivView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchUnivView.swift
+//  StudFile
+//
+//  Created by Александр Плешаков on 24.11.2022.
+//
+
+import SwiftUI
+
+struct SearchUnivView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchUnivView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchUnivView()
+    }
+}
